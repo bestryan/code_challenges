17e3d01b51d37dcf81beecf16f53dc95ecbc5b9a
Ryan Xiao <77907899+bestryan@users.noreply.github.com>
2023-04-13 18:44:25 +1000

    day 3

--- code_record.js
+++ code_record.js
@@ -130,3 +130,42 @@ function booleanToString(b) {
 function validPhoneNumber(phoneNumber) {
   return phoneNumber.match(/^\(\d{3}\)\s{1}\d{3}\-{1}\d{4}$/g) ? true : false;
 }
+
+// Day 3
+
+// Take a Ten Minutes Walk - E
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+function isValidWalk(walk) {
+  let ns = 0;
+  let ew = 0;
+  for (let i = 0; i < walk.length; i++) {
+    if (walk[i] === 'n') {
+      ns += 1;
+    } else if (walk[i] === 's') {
+      ns -= 1;
+    } else if (walk[i] === 'w') {
+      ew += 1;
+    } else if (walk[i] === 'e') {
+      ew -= 1;
+    }
+  }
+  if (walk.length === 10 && (ns === 0) & (ew === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Multiples of 3 or 5 - E
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+// Courtesy of projecteuler.net (Problem 1)
+function solution(number) {
+  let sum = 0;
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) sum += i;
+  }
+  return sum;
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+const os = require('os');
+const server = {
+  type: os.type(),
+  architecture: os.arch(),
+  uptime: os.uptime(),
+};
+console.log(server);
